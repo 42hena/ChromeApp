2122a23935d746966ec4252b7ac96ab92df2dd7e
42hena <somnium1108@gmail.com>
2021-07-31 19:26:27 +0900

    ver1.1

--- ChromeApp/app.js
+++ ChromeApp/app.js
@@ -1,1 +1,14 @@
-alert("hi");
+const a = 5;
+const b = 2;
+let myName = "nico"
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log("hello" + myName)
+
+myName = "nicolas"
+
+console.log("your new name is " + myName)
+//most const some let never var
