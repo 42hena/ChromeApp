5a9a3920dd4825f373ba738c741148c1c2c9336f
42hena <somnium1108@gmail.com>
2021-07-31 19:48:51 +0900

    null,undefined, boolean

--- ChromeApp/app.js
+++ ChromeApp/app.js
@@ -1,14 +1,21 @@
-const a = 5;
-const b = 2;
-let myName = "nico"
+// const a = 5;
+// const b = 2;
+// let myName = "nico"
 
-console.log(a + b)
-console.log(a - b)
-console.log(a * b)
-console.log(a / b)
-console.log("hello" + myName)
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log("hello" + myName)
 
-myName = "nicolas"
+// myName = "nicolas"
 
-console.log("your new name is " + myName)
+// console.log("your new name is " + myName)
 //most const some let never var
+
+const amIFat = null;
+let something = "value"
+console.log(amIFat)
+console.log(something)
+//null은 자연적으로 일어나지 않음 아무것도 없다는 것을 의도적으로 표현한것
+//undefined는 메모리에는 존재하지만 값이 없는 것
