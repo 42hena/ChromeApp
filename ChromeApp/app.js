d2671bcb38f728341dc56e351a79a33df0fdad65
42hena <somnium1108@gmail.com>
2021-08-02 19:15:41 +0900

    preview end

--- ChromeApp/app.js
+++ ChromeApp/app.js
@@ -2,14 +2,15 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleCilck(){
     const clickedClass = "clicked"
-    if (h1.className == clickedClass)
-    {
-        h1.className = "";
-    }
-    else
-    {
-        h1.className = clickedClass;
-    }  
+    // if (h1.classList.contains(clickedClass))
+    // {
+    //     h1.classList.remove(clickedClass);
+    // }
+    // else
+    // {
+    //     h1.classList.add(clickedClass);
+    // }  
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleCilck)
