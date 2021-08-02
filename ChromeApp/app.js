8a349459f2661c057f0c6b52397097dfe32874b2
42hena <somnium1108@gmail.com>
2021-08-02 18:35:36 +0900

    event more

--- ChromeApp/app.js
+++ ChromeApp/app.js
@@ -1,17 +1,38 @@
-const title = document.querySelector("div.hello:first-child h1");
+const h1 = document.querySelector("div.hello:first-child h1");
 // const title = document.getElementById("title")
 
 //console.dir(title)
 function handleCilckTitle(){
-    title.style.color = "blue"
+    h1.style.color = "blue"
 }
 function handleMouseEnter(){
-    title.innerText = "Mouse Enter";
+    h1.innerText = "Mouse Enter";
 }
 
 function handleMouseLeave(){
-    title.innerText = "Mouse Leave";
+    h1.innerText = "Mouse Leave";
 }
-title.addEventListener("click", handleCilckTitle);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier")
+}
+
+function handleWindowOffline(){
+    alert("SOS no WIFI")
+}
+function handleWindowOnline(){
+    alert("WoW")
+}
+//h1.addEventListener("click", handleCilckTitle);
+h1.onclick = handleCilckTitle;//위와 동일
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize)
+window.addEventListener("copy", handleWindowCopy)
+window.addEventListener("offline", handleWindowOffline)
+window.addEventListener("online", handleWindowOnline)
