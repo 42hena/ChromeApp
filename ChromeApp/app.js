66d39ee9676cf656d57a001a25ab480f638df8c5
42hena <somnium1108@gmail.com>
2021-08-02 18:49:25 +0900

    js event

--- ChromeApp/app.js
+++ ChromeApp/app.js
@@ -2,37 +2,14 @@ const h1 = document.querySelector("div.hello:first-child h1");
 // const title = document.getElementById("title")
 
 //console.dir(title)
-function handleCilckTitle(){
-    h1.style.color = "blue"
-}
-function handleMouseEnter(){
-    h1.innerText = "Mouse Enter";
-}
-
-function handleMouseLeave(){
-    h1.innerText = "Mouse Leave";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
+function handleTitleCilck(){
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue")
+        newColor = "tomato";
+    else
+        newColor = "blue";
+    h1.style.color = newColor;
 }
 
-function handleWindowCopy(){
-    alert("copier")
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI")
-}
-function handleWindowOnline(){
-    alert("WoW")
-}
-//h1.addEventListener("click", handleCilckTitle);
-h1.onclick = handleCilckTitle;//위와 동일
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize)
-window.addEventListener("copy", handleWindowCopy)
-window.addEventListener("offline", handleWindowOffline)
-window.addEventListener("online", handleWindowOnline)
+h1.addEventListener("click", handleTitleCilck)
