b3af8bf227fddbcb019771f3c7c00b016e0a8883
42hena <somnium1108@gmail.com>
2021-08-04 15:23:05 +0900

    event 2

--- ChromeApp/app.js
+++ ChromeApp/app.js
@@ -1,10 +1,20 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input")
 
-function onLoginSumbit(tomato){
-    tomato.preventDefault();
+const link = document.querySelector('a');
+
+console.log(link)
+function onLoginSumbit(event){
+    event.preventDefault();
     // console.log(tomato)
     console.log(loginInput.value)
 }
 
+function handleListener(event){
+    console.log(event)
+    event.preventDefault();
+    // alert("click")
+}
+
 loginForm.addEventListener("submit", onLoginSumbit)
+link.addEventListener("click",handleListener)
