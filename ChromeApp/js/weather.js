94e3452f1ac2c1cbbd7c460b9424787244d43c94
42hena <somnium1108@gmail.com>
2021-08-05 23:22:03 +0900

    Geolocation

---
+++ ChromeApp/js/weather.js
@@ -0,0 +1,11 @@
+function onGeOk(position){
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    console.log("You live in", lat, lng);
+}
+
+function onGeoError(){
+    alert("Can't find you. No weather for you.")
+}
+
+navigator.geolocation.getCurrentPosition(onGeOk, onGeoError)
